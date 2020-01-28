a7a5990a0350d74ecf583e16ec391cf7b4e03ce5
kasiselvamk <kasiselvamk@gmail.com>
2020-01-28 21:42:48 +0530

    Selection Sort

---
+++ Algo-1/src/SelectionSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int [] a = {10,9,8,7,6,5,4,3,2,1,0};
+		int minIndex = 0;
+		int tmp;
+		  for (int i=0;i<a.length;i++) {
+			   minIndex = i;//
+			   for (int j=i;j<a.length;j++) {
+				   if(a[j]<a[minIndex])minIndex = j;
+			   }
+			   tmp =  a[i];
+			   a[i] = a[minIndex] ;
+			   a[minIndex] = tmp;
+		  }
+		  System.out.println(Arrays.toString(a));
+	}
+
+}
