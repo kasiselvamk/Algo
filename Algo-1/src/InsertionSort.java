0861d39e544fd16c907f8f0d2a9794cd074954ec
kasiselvamk <kasiselvamk@gmail.com>
2020-01-28 22:08:19 +0530

    c

--- Algo-1/src/InsertionSort.java
+++ Algo-1/src/InsertionSort.java
@@ -6,7 +6,7 @@ public class InsertionSort {
 		int [] a = {10,9,8,7,6,5,4,3,2,1,0};
 		int tmp;
 		  for (int i=0;i<a.length;i++) {
-			   for (int j=i;j>=1;j--) {//
+			   for (int j=i;j>0;j--) {//
 				   if(a[j]<a[j-1]) {//
 					   tmp =  a[j];//
 					   a[j] = a[j-1];//
