42ec55d60bea4d901eb35d83de3f963d67d9a417
kasiselvamk <kasiselvamk@gmail.com>
2020-01-28 22:05:44 +0530

    commit it

---
+++ Algo-1/src/InsertionSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int [] a = {10,9,8,7,6,5,4,3,2,1,0};
+		int tmp;
+		  for (int i=0;i<a.length;i++) {
+			   for (int j=i;j>=1;j--) {//
+				   if(a[j]<a[j-1]) {//
+					   tmp =  a[j];//
+					   a[j] = a[j-1];//
+					   a[j-1] = tmp;//
+				   }
+			   }
+		  }
+		  System.out.println(Arrays.toString(a));
+	}
+}
