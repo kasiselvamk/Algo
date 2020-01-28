0ee343a9b6f9058371aa097f83211de7c7e4fbd5
kasiselvamk <kasiselvamk@gmail.com>
2020-01-28 23:47:09 +0530

    cmt

---
+++ Algo-1/src/com/hackerrank/Solution.java
@@ -0,0 +1,55 @@
+package com.hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+    	 n = ar.length;
+    	 int sum=0;
+    	 Arrays.sort(ar);
+    	 System.out.println(Arrays.toString(ar));
+    	 for(int i=n-1;i>0;i--) {
+    		 System.out.println(ar[i]);
+    		 if(ar[i] == ar[i-1]) {i--;sum++ ;}
+    	 }
+    	
+		return sum;
+    
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/Users/kasiselvamk/git/Algo-1/Algo-1/out.txt"));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+        System.out.println(result);
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
